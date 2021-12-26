952857afa4b80dc4436ac7badc2c87be326238cc
private <toruuetani@gmail.com>
2021-12-26 15:16:08 +0900

    Add eslint, prettier configs.

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "eslint-config-prettier",
+    "eslint-config-prettier/vue",
+    "eslint-config-prettier/@typescript-eslint",
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {},
+};
